a44f87d40dfd86f0e937e67259ccb76631a2b8bc
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-11 13:32:56 -0800

    Problem3 complete in LinqProblems.cs.

--- PracticeProblemsLINQ/LinqProblems.cs
+++ PracticeProblemsLINQ/LinqProblems.cs
@@ -75,21 +75,17 @@ namespace PracticeProblemsLINQ
         //Using LINQ, write a method that takes in a list of customers and returns the lone customer who has the name of Mike. 
         public static Customer RunProblem3(List<Customer> customers)
         {
-            var qCustomers = customers.AsQueryable();
+
+            //customers.AsQueryable();
             //code
             var mikeQuery =
-               from c in QCustomers
+               (from c in customers
                 where c.FirstName.Contains("Mike")
-                select c.First();
+                select c).First();
 
-            Customer cust = mikeQuery[0];
-
-
-
-
-
+           
             //return
-            return cust;
+            return mikeQuery;
 
         }
         #endregion
